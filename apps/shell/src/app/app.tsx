d2314b6fb8cfc89c0ef018e1644434ee49f368c5
Chris Kettenbach <christopherk@logic2020.com>
2022-09-30 11:07:33 -0700

    mfe testing initial setup

---
+++ apps/shell/src/app/app.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
+const About = React.lazy(() => import('about/Module'));
+
+const Shop = React.lazy(() => import('shop/Module'));
+
+const Cart = React.lazy(() => import('cart/Module'));
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<About />} />
+
+        <Route path="/shop" element={<Shop />} />
+
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </React.Suspense>
+  );
+}
+
+export default App;
---
+++ apps/shop/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <>
+      <h1>Shop</h1>
+      <div />
+    </>
+  );
+}
+
+export default App;
